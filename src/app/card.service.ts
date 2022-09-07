90009ec4e94975d2730350f7fd779894bfee6439
Coltahhh <harrisonphillips25@gmail.com>
2022-09-06 19:47:45 -0500

    changed homepage text as well as card functionality

--- src/app/card.service.ts
+++ src/app/card.service.ts
@@ -25,23 +25,23 @@ export class CardService {
 		}, {
 			name: "Judgement",
 			image: "https://cdn.discordapp.com/attachments/751981148875390997/1014397648628891708/Judgement.jpg",
-			description: "If you stole a shot at any point of the game, you lose all of your shots. If you didn’t steal, you will be awarded 1 shot"
+			description: "If you stole a shot at any point of the game, you reset back to 5 shots. If you didn’t steal, you will be awarded 1 shot"
 		}, {
 			name: "The Star",
 			image: "https://cdn.discordapp.com/attachments/751981148875390997/1014397505540198400/TheStar.jpg",
-			description: "If you draw this card at any point in the game you can save yourself if you reach over 5 shots by resetting back to 4 shots"
+			description: "If you draw this card at any point in the game you can save yourself if you reach over 10 shots by resetting back to 5 shots"
 		}, {
 			name: "The Wheel Of Fortune",
 			image: "https://cdn.discordapp.com/attachments/751981148875390997/1014397223062216755/WheelofFortune.jpg",
-			description: "Roll the die. If its even you get 2 shots, odd you lose 1 shot, matching pair you get 3 shots"
+			description: "Roll the die twice. If its even you get 2 shots, odd you lose 1 shot, matching pair you get 3 shots"
 		}, {
 			name: "The Hanged Man",
 			image: "https://cdn.discordapp.com/attachments/751981148875390997/1014397286429765642/TheHangedMan.jpg",
-			description: "Reset to 0 shots"
+			description: "Reset to 5 shots"
 		}, {
 			name: "Death",
 			image: "https://cdn.discordapp.com/attachments/751981148875390997/1014397326082707537/Death.jpg",
-			description: "Death – Add 4 shots"
+			description: "Add 4 shots"
 		}, {
 			name: "Temperance",
 			image: "https://cdn.discordapp.com/attachments/751981148875390997/1014397356483026944/Temperance.jpg",
@@ -49,7 +49,7 @@ export class CardService {
 		}, {
 			name: "The Devil",
 			image: "https://cdn.discordapp.com/attachments/751981148875390997/1014397438649454612/TheDevil.jpg",
-			description: "Shot of water L"
+			description: "Add 2 shots"
 		}, {
 			name: "The Tower",
 			image: "https://cdn.discordapp.com/attachments/751981148875390997/1014397471851548724/TheTower.jpg",
@@ -77,7 +77,7 @@ export class CardService {
 		}, {
 			name: "The Lovers",
 			image: "https://cdn.discordapp.com/attachments/751981148875390997/1014397001590374451/TheLovers.jpg",
-			description: "Swap shots with your opponent"
+			description: "Swap shot amountrgrgr with your opponent"
 		}, {
 			name: "Justice",
 			image: "https://cdn.discordapp.com/attachments/751981148875390997/1014397037959188501/Justice.jpg",
